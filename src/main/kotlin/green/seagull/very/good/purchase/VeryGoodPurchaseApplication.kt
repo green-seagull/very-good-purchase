ce9b75de258d6c9a558367a90b0242a2c4ab161f
green-seagull <9652156+green-seagull@users.noreply.github.com>
2021-02-04 07:40:35 +1100

    Created project from Spring init

---
+++ src/main/kotlin/green/seagull/very/good/purchase/VeryGoodPurchaseApplication.kt
@@ -0,0 +1,11 @@
+package green.seagull.very.good.purchase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VeryGoodPurchaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<VeryGoodPurchaseApplication>(*args)
+}
